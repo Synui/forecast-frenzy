5dfabc3ed3c111b36b520b26860be9e6abd694c5
Synui <jonathon.schoenholz@yahoo.com>
2021-12-12 12:08:16 -0500

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-var citiesEl = document.querySelector("#cities")
+var cities = [];
+var searchFormEl = document.querySelector("#search-city-form");
+var searchCityEl = document.querySelector("#search-city");
+var citiesEl = document.querySelector("#cities");
+var currentCityEL = document.querySelector("#current-city");
+var currentDayEl = document.querySelector("#current-day");
+var fiveDayForecastEl = document.querySelector("#five-day-forecast");
+var fiveDayEl = document.querySelector("#five-day");
 
-// var getCities = function(city)
